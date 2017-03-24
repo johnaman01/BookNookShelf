230d4f20ef2b1c6f158505006188df809723cdce
John Amann <johnaman01@gmail.com>
2017-03-24 17:54:19 -0400

    Named app

--- src/RabookShelf/Models/Author.cs
+++ src/RabookShelf/Models/Author.cs
@@ -7,6 +7,13 @@ namespace RabookShelf.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string AuthorDisplayText
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         
         //public string Pseudonym { get; set; }
         //public string Nationality { get; set; }
